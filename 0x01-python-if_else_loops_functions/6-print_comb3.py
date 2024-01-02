041d088b75e8a5ba4f1f9c2da52ab4e6411a0f5e
mclakes <lekanhayo@gmail.com>
2024-01-02 10:43:39 -0800

    work in progress

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 9):
+    for y in range(i + 1, 10):
+        if x == 8:
+            print("{}{}".format(x, y))
+        else:
+            print("{}{}".formatxi, y), end=", ")
