3f8f32ae2dddd13d726cb5f0a2803a3e1e61a6af
mclakes <lekanhayo@gmail.com>
2024-01-02 11:05:41 -0800

    corr12

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python4
-for x in range(0, 9):
-    for y in range(x + 1, 10):
-        if x == 8:
-            print("{}{}".format(x, y))
-        else:
-            print("{}{}".format (x, y), end=", ")
+#!/usr/bin/python3
+n = 1
+for i in range(0, 8):
+    for j in range(n, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+    n = n + 1
+print("89")
