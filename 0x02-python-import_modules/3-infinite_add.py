1d45619b588e86172c1b4646ae0ff4cbb84a70f3
mclakes <lekanhayo@gmail.com>
2024-01-04 11:59:46 -0800

    corr

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from sys import argv
-
-def main():
-    sum = 0
-    for n in range(1, len(argv)):
-        sum += int(argv[n])
-    print(sum)
+import sys
 
 if __name__ == "__main__":
-    main()
+    sum = 0
+    if len(sys.argv) > 1:
+        for n in range(1, len(sys.argv)):
+            sum += int(sys.argv[n])
+        print("{:d}".format(sum))
+    else:
+        print("{:d}".format(0))
