a14817d1622d876e1f6a05c6c4175f2435d4a1ca
mclakes <lekanhayo@gmail.com>
2024-01-10 14:11:41 -0800

    readme

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
+    for n in range(len(new_list)):
+        if new_list[n] == search:
+            new_list[n] = replace
     return (new_list)
