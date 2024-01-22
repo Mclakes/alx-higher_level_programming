face06df663385a1cb46283cd530b0de748332e4
mclakes <lekanhayo@gmail.com>
2024-01-22 14:33:28 -0800

    checking

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
     my_list_3 = []
+
     for i in range(list_length):
         try:
             l3 = my_list_1[i] / my_list_2[i]
