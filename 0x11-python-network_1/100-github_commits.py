1bc68d4417df7470297951d31fb9749a8163d79c
Olalekan Bankole <lekanhayo@gmail.com>
2024-04-29 23:05:43 +0100

    init

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{owner}/{repo}/commits"
+    uri = url.format(owner=sys.argv[2], repo=sys.argv[1])
+    r = requests.get(uri).json()
+    for _commit in r[:10]:
+        author = _commit.get("commit").get("author").get("name")
+        sha = _commit.get("sha")
+        print("{}: {}".format(sha, author))
